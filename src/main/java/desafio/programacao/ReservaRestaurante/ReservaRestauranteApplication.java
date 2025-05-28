0f72fad731926be8676c5bdfbf83eeddafffb9e3
César Rodrigues <cesarnogueirarodrigues1200@gmail.com>
2025-05-27 23:25:26 -0300

    Primeiro commit

---
+++ src/main/java/desafio/programacao/ReservaRestaurante/ReservaRestauranteApplication.java
@@ -0,0 +1,13 @@
+package desafio.programacao.ReservaRestaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaRestauranteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/desafio/programacao/ReservaRestaurante/ReservaRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package desafio.programacao.ReservaRestaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
