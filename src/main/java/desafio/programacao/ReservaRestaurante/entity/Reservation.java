a69b84d87c511dc083c1820e182f292a67a55bdc
César Rodrigues <cesarnogueirarodrigues1200@gmail.com>
2025-05-31 01:02:38 -0300

    Criação das entidades e mapeamento JPA inicial para futura integração com o banco de dados, atualização do .gitignore

---
+++ src/main/java/desafio/programacao/ReservaRestaurante/entity/Reservation.java
@@ -0,0 +1,10 @@
+package desafio.programacao.ReservaRestaurante.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Table(name = "Reservation")
+@Entity(name = "Reservation")
+public class Reservation {
+
+}
---
+++ src/main/java/desafio/programacao/ReservaRestaurante/entity/RestaurantTable.java
@@ -0,0 +1,13 @@
+package desafio.programacao.ReservaRestaurante.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Table(name = "RestaurantTable")
+@Entity(name = "RestaurantTable")
+public class RestaurantTable {
+
+}
+
+
+
